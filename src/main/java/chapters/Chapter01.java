14b793a833ddbbe085e0bfb0f31daa559ebeb40f
04.000303 <ricardo.mesquita@livelo.com.br>
2020-11-22 21:13:11 -0300

    Adding chapter01 rest assured test

---
+++ src/main/java/chapters/Chapter01.java
@@ -0,0 +1,16 @@
+package chapters;
+
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Chapter01 {
+
+    @Test
+    public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills(){
+
+        given().when().get("http://zippopotam.us/us/90210").then().assertThat().body("places[0].'place name'", equalTo("Beverly Hills"));
+
+    }
+}
