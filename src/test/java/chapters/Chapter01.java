587a09a13e8ad91c44abfcd227cf022892315c90
04.000303 <ricardo.mesquita@livelo.com.br>
2020-11-22 21:53:08 -0300

    Adding Chapter02 tests

--- src/test/java/chapters/Chapter01.java
+++ src/test/java/chapters/Chapter01.java
@@ -8,9 +8,10 @@ import static org.hamcrest.Matchers.equalTo;
 public class Chapter01 {
 
     @Test
-    public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills(){
+    public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills() {
 
-        given().when().get("http://zippopotam.us/us/90210").then().assertThat().body("places[0].'place name'", equalTo("Beverly Hills"));
-
+        given().
+        when().
+        get("http://zippopotam.us/us/90210").then().assertThat().body("places[0].'place name'", equalTo("Beverly Hills"));
     }
 }
